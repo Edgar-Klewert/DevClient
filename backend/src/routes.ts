02c217a4da602ffc2143989301128e4c98a162b4
Edgar-Klewert <edgarklewert66@gmail.com>
2024-05-16 16:33:37 -0300

    list customers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from 'fastify';
 import { CreateCustomerControllers } from './controllers/CreateCustomerControllers';
+import { ListCustomersController } from "./controllers/ListCustomersController"
 
 export async function routes(fastify: FastifyInstance) {
   fastify.get("/teste", async (request: FastifyRequest, reply: FastifyReply) => {
@@ -8,5 +9,9 @@ export async function routes(fastify: FastifyInstance) {
 
   fastify.post("/customer", async (request: FastifyRequest, reply: FastifyReply)=>{
     return new CreateCustomerControllers().handle(request, reply)
+  })
+
+  fastify.get("/customers", async (request: FastifyRequest, reply: FastifyReply)=>{
+    return new ListCustomersController().handle(request, reply)
   })
 }
